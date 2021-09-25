2b0777d9ed4a426aa714b15ced4a1d4289019d5c
Fran Urbano <viraltux@gmail.com>
2021-09-25 20:32:06 +0200

    update splot test

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,22 +6,19 @@ using Test
     x = rand(100)
 
     #splot
-    #px = splot(x;title="SPlot")
-    # @test size(px) == (9,12)
+    px = splot(x;title="SPlot", plot=false)
+    @test size(px) == (9,12)
 
-    # px = splot(x,"day";title="SPlot")
-    # @test size(px) == (15,17)
+    px = splot(x,"day";title="SPlot", plot=false)
+    @test size(px) == (15,7)
     
-    # px = splot(x,"quarter",title="SPlot")
-    # @test size(px) == (25,4)
+    px = splot(x,"quarter",title="SPlot", plot=false)
+    @test size(px) == (25,4)
     
-    # splot(x,"month",title="SPlot")
-    # @test size(px) == (9,12)
+    px = splot(x,"month",title="SPlot", plot=false)
+    @test size(px) == (9,12)
     
-    # splot(x,10,title="SPlot")
-    # @test size(px) == (10,10)
-    
-    px = splot(x,10,title="SPlot",plot=false)
+    px = splot(x,10,title="SPlot", plot=false)
     @test size(px) == (10,10)
     
 end
