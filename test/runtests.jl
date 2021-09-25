c07f30b104c0651f45b8b73b2cbd84e3dc788e50
Fran Urbano <viraltux@gmail.com>
2021-09-25 19:07:08 +0200

    splot test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,10 +4,14 @@ using Test
 @testset "ForecastPlots.jl" begin
 
     x = rand(100)
-    splot(x,title="SPlot")
-    splot(x,"day",title="SPlot")
-    splot(x,"quarter",title="SPlot")
-    splot(x,"month",title="SPlot")
-    splot(x,10,title="SPlot")
+
+    #splot
+    px = splot(x;title="SPlot")
+    @test size(px) == (9,12)
+    splot(x,"day";title="SPlot"); @test true
+    splot(x,"quarter",title="SPlot"); @test true
+    splot(x,"month",title="SPlot"); @test true
+    splot(x,10,title="SPlot"); @test true
+    splot(x,10,title="SPlot",plot=false); @test true
     
 end
